69aa2c08e300125b82bc205afd00f79e959a6469
Johan Revilla <johan.revilla.92@gmail.com>
2018-10-20 18:31:30 -0300

    definiendo la interfaz FiguraGeometrica2D

---
+++ src/modelos/figura_geometrica_2d/FiguraGeometrica2D.h
@@ -0,0 +1,13 @@
+#ifndef MODELOS_FIGURA_GEOMETRICA_2D_FIGURAGEOMETRICA2D_H_
+#define MODELOS_FIGURA_GEOMETRICA_2D_FIGURAGEOMETRICA2D_H_
+
+class FiguraGeometrica2D{
+public:
+	FiguraGeometrica2D(){};
+	virtual double calcularSuperficie() = 0;
+	virtual double calcularPerimetro() = 0;
+	virtual ~FiguraGeometrica2D(){};
+};
+
+
+#endif
